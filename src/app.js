c406375274220729b9453399b7288fb06cacd064
CrysPy-SAM <ssmishra9918@gmail.com>
2025-10-21 23:57:23 +0530

    db connect

--- src/app.js
+++ src/app.js
@@ -9,14 +9,16 @@ import cors from "cors";
  
 const app = express();
 const server = createServer(app);
+const io = new Server(server)
 
-
+app.set("port", (process.env.PORT || 8000))
 app.get("/home", (req, res)=>{
     return res.json({"hello": "World"})
 });
 
 const start = async () =>{
-    app.listen(8000, ()=>{
+    const connectionDb = await mongoose.connect("mongodb+srv://Satyam:Satyam%40123@cluster0.mdrsd2c.mongodb.net/testDB")
+    server.listen(app.get("port"), ()=>{
         console.log("LISENING ON PORT 8000")
     });
 }
