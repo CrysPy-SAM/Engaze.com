0f8ed0d37cdd2b9e192e5f35df9a74e0b87a217f
CrysPy-SAM <ssmishra9918@gmail.com>
2025-10-21 22:41:19 +0530

    app.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+
+import cors from "cors";
+ 
+const app = express();
+
+
+app.get("/home", (req, res)=>{
+    return res.json({"hello": "World"})
+});
+
+const start = async () =>{
+    app.listen(8000, ()=>{
+        console.log("LISENING ON PORT 8000")
+    });
+}
+
+start();
