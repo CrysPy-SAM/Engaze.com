109885e1c978a753ed6a7a2bd2ee5cdb049deb54
CrysPy-SAM <ssmishra9918@gmail.com>
2025-10-22 15:39:19 +0530

    controlller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+const register = async (req, res) => {
+    const { name, username, password } = req.body;
+
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(httpStatus.FOUND).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name: name,
+            username: username,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        res.status(httpStatus.CREATED).json({ message: "User Registered" })
+
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+
+}
